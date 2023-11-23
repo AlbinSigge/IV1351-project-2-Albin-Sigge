23d5031c5e8fa175bcc7438df92e4d7927d4705b
Albin <albin@sigge.cc>
2023-11-23 18:32:46 +0100

    initial commit

---
+++ lessons.sql
@@ -0,0 +1,4 @@
+INSERT INTO lesson (date, duration, skill_level, pricing_id, instructor_id) VALUES ('2023-11-23 17:00:00', 60, 'Beginner', 2, 3);
+INSERT INTO lesson (date, duration, skill_level, pricing_id, instructor_id) VALUES ('2023-11-23 17:00:00', 60, 'Beginner', 8, 20);
+INSERT INTO lesson (date, duration, skill_level, pricing_id, instructor_id) VALUES ('2023-11-23 17:00:00', 60, 'Advanced', 7, 15);
+
