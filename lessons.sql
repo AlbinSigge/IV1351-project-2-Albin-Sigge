5c77fe9159961ae5a49dabf38d5f6f399434f7a7
Albin <albin@sigge.cc>
2023-11-23 18:41:18 +0100

    test commit

--- lessons.sql
+++ lessons.sql
@@ -2,3 +2,5 @@ INSERT INTO lesson (date, duration, skill_level, pricing_id, instructor_id) VALU
 INSERT INTO lesson (date, duration, skill_level, pricing_id, instructor_id) VALUES ('2023-11-23 17:00:00', 60, 'Beginner', 8, 20);
 INSERT INTO lesson (date, duration, skill_level, pricing_id, instructor_id) VALUES ('2023-11-23 17:00:00', 60, 'Advanced', 7, 15);
 
+
+
